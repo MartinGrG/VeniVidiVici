80298de4d1ce3707c956690919824fe172ad91cd
amark <alexismarkiewicz020@gmail.com>
2024-05-29 12:13:34 -0400

    J'ai ajouté la vérification des réponses données par l'utilisateur. Peu importe ce qu'on entre le code tourne maintenant.

--- Interface.py
+++ Interface.py
@@ -7,15 +7,41 @@ def verifier_int(chaine):
     """Cette fonction sert à vérifier qu'une chaîne de caractère contient un entier"""
     # Entrée : chaîne de caractère
     # Sorite : booléen
-    liste_int = ['-','0','1','2','3','4','5','6','7','8','9']
-    for i in chaine:
-        if i not in liste_int:
+    liste_int = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for idx, elem in enumerate(chaine):
+        if elem not in liste_int:
+            return False
+        elif elem == '-' and idx != 0:
             return False
     return True
 
 
+def verifier_binaire(choix1, choix2):
+    """Cette fonction sert à s'assurer que l'utilisateur a entré une réponse parmi 2 choix"""
+    # Entrée : des chaînes de caractères
+    # Sortie : une chaîne de caractère
+    choix = input("--> ")
+    taille_fenetre = 39
+    espace_gauche1 = (taille_fenetre - len(choix1)) // 2
+    espace_droite1 = taille_fenetre - len(choix1) - espace_gauche1
+    espace_gauche2 = (taille_fenetre - len(choix2)) // 2
+    espace_droite2 = taille_fenetre - len(choix2) - espace_gauche2
+    liste_choix = [choix1, choix2]
+    while choix not in liste_choix:
+        print("| La réponse entrée n'est pas correcte. |")
+        print("|                                       |")
+        print("|       Veuillez choisir entre :        |")
+        print(f"|{espace_gauche1 * ' '}{choix1}{espace_droite1 * ' '}|")
+        print(f"|{espace_gauche2 * ' '}{choix2}{espace_droite2 * ' '}|")
+        print("|                                       |")
+        choix = input("--> ")
+    return choix
+
+
 def verifier_clef():
-    clef = verifier_binaire('oui','non')
+    """Cette fonction sert à vérifier que l'utilisateur entre une clef valide s'il en a une"""
+    # Sortie : un entier ou une chaîne de caractère 'non' si l'utilisateur n'a pas de clé
+    clef = verifier_binaire('oui', 'non')
     if clef == 'oui':
         print("|                                       |")
         print("|        Quelle est votre clef ?        |")
@@ -29,29 +55,6 @@ def verifier_clef():
         return int(valeur_clef)
     else:
         return 'non'
-
-
-def verifier_binaire(choix1, choix2):
-    """Cette fonction sert à s'assurer que l'utilisateur a entré une réponse parmi 2 choix"""
-    # Entrée : des chaînes de caractères
-    # Sortie : une chaîne de caractère
-    choix = input("--> ")
-    taille_fenetre = 39
-    espace = ' '
-    espace_gauche1 = (taille_fenetre - len(choix1)) // 2
-    espace_droite1 = taille_fenetre - len(choix1) - espace_gauche1
-    espace_gauche2 = (taille_fenetre - len(choix2)) // 2
-    espace_droite2 = taille_fenetre - len(choix2) - espace_gauche2
-    liste_choix = [choix1, choix2]
-    while choix not in liste_choix:
-        print("| La réponse entrée n'est pas correcte. |")
-        print("|                                       |")
-        print("|       Veuillez choisir entre :        |")
-        print(f"|{espace_gauche1 * ' '}{choix1}{espace_droite1 * ' '}|")
-        print(f"|{espace_gauche2 * ' '}{choix2}{espace_droite1 * ' '}|")
-        print("|                                       |")
-        choix = input("--> ")
-    return choix
 
 
 def interface():
@@ -72,7 +75,7 @@ def interface():
 
     # On vérifie d'abord 'non' parce que clef is int ne fonctionne
     # pas donc impossible de verifier si on a un entier ou pas
-    else: # On a la clef de cryptage : mode normal
+    else:  # On a la clef de cryptage : mode normal
         print("|    Vous avez une clef d'encryptage    |")
         print("|      vous entrez en mode normal       |")
 
@@ -89,7 +92,7 @@ def interface():
             fichier_nom = input("--> ")
             texte = lecture(fichier_nom)
 
-        else: # extraire le texte via un input
+        else:  # extraire le texte via un input
 
             print("|       entrer le texte ci-dessous      |")
             texte = input("--> ")
@@ -97,12 +100,11 @@ def interface():
         print("|   Souhaitez vous encoder ou decoder   |")
         print("|                                       |")
         print("|  encrypter(encry)   decrypter(decry)  |")
-        choix = verifier_binaire('encry','decry')
+        choix = verifier_binaire('encry', 'decry')
 
         if choix == "encry":
-            ecriture(crypter(texte,clef))
+            ecriture(crypter(texte, clef))
             return crypter(texte, clef)
         else:
             ecriture(crypter(texte, clef))
             return decrypter(texte, clef)
-
